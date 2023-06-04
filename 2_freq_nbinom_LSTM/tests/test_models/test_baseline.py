1d510188916714b1e11a032ce2c5e3835547eb4a
Jose Peeterson <josepeeterson.er@ntu.edu.sg>
2023-06-04 16:07:48 +0800

    Restructured, Reorganized and Refactored the code. Currently implementing visualization and analysis code

--- 2_freq_nbinom_LSTM/tests/test_models/test_baseline.py
+++
@@ -1,7 +0,0 @@
-from pytorch_forecasting import Baseline
-
-
-def test_integration(multiple_dataloaders_with_covariates):
-    dataloader = multiple_dataloaders_with_covariates["val"]
-    Baseline().predict(dataloader, fast_dev_run=True)
-    repr(Baseline())
